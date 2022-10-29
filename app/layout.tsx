07e76376ac6d2cf2500a5871030a8906dbafc56b
Nick Seman <nick@nickseman.com>
2022-10-29 10:43:49 -0700

    feat: convert to app dir

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>dual-type-chart</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
