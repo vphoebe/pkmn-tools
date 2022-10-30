932e632d1ef98bd1bfdad9c7dd516fbf1eac3521
Nick Seman <nick@nickseman.com>
2022-10-30 00:43:25 -0700

    feat: add a sprite

--- components/DefendingSection.tsx
+++ components/DefendingSection.tsx
@@ -3,6 +3,7 @@ import { TypeSelector } from "./TypeSelector";
 import Select from "react-select";
 import * as React from "react";
 import { Generation, getGenNumberFromUrl } from "../lib/getTypes";
+import Image from "next/image";
 
 interface DefendingSectionProps {
   typeList: string[];
@@ -15,10 +16,10 @@ interface DefendingSectionProps {
 async function getPokemonTypesByName(
   name: string,
   generation: Generation
-): Promise<{ type1: string; type2: string }> {
+): Promise<{ type1: string; type2: string; spriteUrl: string }> {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const json = await res.json();
-  const { types, past_types } = json;
+  const { types, past_types, sprites } = json;
   if (generation !== "current" && past_types.length) {
     const oldTypeChange = past_types[0];
     const oldTypeGenNumber = getGenNumberFromUrl(oldTypeChange.generation.url);
@@ -30,12 +31,14 @@ async function getPokemonTypesByName(
         type2: oldTypeChange.types[1]
           ? oldTypeChange.types[1].type.name
           : "none",
+        spriteUrl: sprites.front_default,
       };
     }
   }
   return {
     type1: types[0].type.name,
     type2: types[1] ? types[1].type.name : "none",
+    spriteUrl: sprites.front_default,
   };
 }
 
@@ -49,6 +52,10 @@ function PokemonSelector({
   setPokemon: any;
 }) {
   const styles = {
+    container: (provided: any) => ({
+      ...provided,
+      flex: "1",
+    }),
     option: (provided: any) => ({
       ...provided,
       color: "black",
@@ -56,12 +63,15 @@ function PokemonSelector({
   };
   const options = pokemonList.map((p) => ({ label: p.name, value: p.name }));
   return (
-    <Select
-      styles={styles}
-      options={options}
-      value={pokemon}
-      onChange={(val) => setPokemon(val)}
-    />
+    <div className="flex flex-1 h-full items-center">
+      <Select
+        styles={styles}
+        options={options}
+        value={pokemon}
+        onChange={(val) => setPokemon(val)}
+        isClearable
+      />
+    </div>
   );
 }
 
@@ -73,24 +83,39 @@ export function DefendingSection({
   gen,
 }: DefendingSectionProps) {
   const [pokemon, setPokemon] = React.useState<{ value: string } | null>(null);
+  const [spriteUrl, setSpriteUrl] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     if (pokemon) {
       getPokemonTypesByName(pokemon.value, gen).then((obj) => {
         setDefenseTypes([obj?.type1 ?? "none", obj?.type2 ?? "none"]);
+        setSpriteUrl(obj.spriteUrl);
       });
+    } else {
+      setDefenseTypes(["none", "none"]);
+      setSpriteUrl(null);
     }
   }, [pokemon, setDefenseTypes, gen]);
 
   return (
     <div>
-      <div className=" w-full text-center mb-4">Defending:</div>
-      <div className="mb-4">
+      <div className="w-full text-center mb-4">Defending:</div>
+      <div className="mb-4 flex h-12 items-center">
         <PokemonSelector
           pokemonList={pokemonList}
           pokemon={pokemon}
           setPokemon={setPokemon}
         />
+        <div className="w-12 h-full ml-2 bg-slate-400 rounded-sm">
+          {spriteUrl ? (
+            <Image
+              alt={pokemon?.value ?? ""}
+              src={spriteUrl}
+              width={48}
+              height={48}
+            />
+          ) : null}
+        </div>
       </div>
       <div className="flex mb-4 w-full gap-x-2 items-center">
         <TypeSelector
--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/PokeAPI/sprites/master/sprites/pokemon/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
