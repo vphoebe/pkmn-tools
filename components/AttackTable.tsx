278a1b422074ecc208290e4574b0dc4402865ca0
Nick Seman <nick@nickseman.com>
2022-10-29 23:08:34 -0700

    feat: sort by multiplier value desc

--- components/AttackTable.tsx
+++ components/AttackTable.tsx
@@ -22,33 +22,50 @@ function MultiplierCell({ value }: { value: number }) {
   );
 }
 
+function AttackRow({
+  attackName,
+  multiplier,
+}: {
+  attackName: string;
+  multiplier: number;
+}) {
+  return multiplier !== 1 ? (
+    <tr key={`${attackName}-row`}>
+      <td
+        className={`bg-${attackName} font-bold text-right py-2 pr-2 ${globalCellClasses}`}
+      >
+        {attackName}
+      </td>
+      <MultiplierCell key={`${attackName}-cell`} value={multiplier} />
+    </tr>
+  ) : null;
+}
+
 export function AttackTable({
   data,
   typeList,
   defenseName1,
   defenseName2,
 }: AttackTableProps) {
+  const attackRowValues = React.useMemo(() => {
+    const values = typeList.map((attackName) => {
+      const multiplier = getMultiplier(data, attackName, [
+        defenseName1,
+        defenseName2,
+      ]);
+      return { attackName, multiplier };
+    });
+    return values.sort((a, b) => b.multiplier - a.multiplier);
+  }, [data, defenseName1, defenseName2, typeList]);
+
   return (
     <React.Fragment>
       <div className="font-mono w-full text-center mb-4">Attacking:</div>
       <table className="table-auto w-full h-full font-mono">
         <tbody>
-          {typeList.map((attackName) => {
-            const multiplier = getMultiplier(data, attackName, [
-              defenseName1,
-              defenseName2,
-            ]);
-            return multiplier !== 1 ? (
-              <tr key={`${attackName}-row`}>
-                <td
-                  className={`bg-${attackName} font-bold text-right py-2 pr-2 ${globalCellClasses}`}
-                >
-                  {attackName}
-                </td>
-                <MultiplierCell key={`${attackName}-cell`} value={multiplier} />
-              </tr>
-            ) : null;
-          })}
+          {attackRowValues.map((row) => (
+            <AttackRow key={row.attackName} {...row} />
+          ))}
         </tbody>
       </table>
     </React.Fragment>
